6bee291309908ff12b2512597ed69e9540096349
Teroxlab <emmanuels820@gmail.com>
2022-07-02 02:09:58 +0100

    second

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - main block
  * Description: Get a random number and print the number
@@ -11,6 +10,7 @@
 int main(void)
 {
 	int n;
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n == 0)
